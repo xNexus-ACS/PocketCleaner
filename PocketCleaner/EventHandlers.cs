567b62ca0df491bf9601310f45c025d5f7b070cb
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-07-16 00:25:49 +0200

    lol ez ploogin

---
+++ PocketCleaner/EventHandlers.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.Events.EventArgs;
+using PlayerStatsSystem;
+
+namespace PocketCleaner
+{
+    public class EventHandlers
+    {
+        public void OnDying(DyingEventArgs ev)
+        {
+            if (ev.Handler.Base is UniversalDamageHandler universal)
+            {
+                if (universal.TranslationId == DeathTranslations.PocketDecay.Id)
+                {
+                    ev.Target.Ammo.Clear();
+                    ev.Target.Inventory.SendAmmoNextFrame = true;
+                    ev.Target.ClearInventory();
+                }
+            }
+        }
+    }
+}
---
+++ PocketCleaner/MainClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace PocketCleaner
+{
+    public class MainClass : Plugin<Config>
+    {
+        public override string Author { get; } = "xNexusACS";
+        public override string Name { get; } = "PocketCleaner";
+        public override string Prefix { get; } = "pocketcleaner";
+        public override Version Version { get; } = new Version(0, 1, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 2, 2);
+        
+        public EventHandlers EventHandlers { get; private set; }
+        
+        public override void OnEnabled()
+        {
+            EventHandlers = new EventHandlers();
+
+            Exiled.Events.Handlers.Player.Dying += EventHandlers.OnDying;
+            
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Dying -= EventHandlers.OnDying;
+            
+            EventHandlers = null;
+            base.OnDisabled();
+        }
+    }
+}
